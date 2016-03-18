a5a1d2f376cf0479c7dad53212ca465d6cd9c60b
Mihai Balaceanu <gizm0bill@gmail.com>
2016-03-18 16:40:52 +0200

    forgot to add typings references link file

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/systemjs/index.d.ts" />
+/// <reference path="../node_modules/zone.js/dist/zone.js.d.ts" />
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+
